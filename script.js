26ff3a6c80b1023346292c4b5c9966569b01d414
Flight <andakurogideon99@gmail.com>
2026-05-10 07:31:58 +0100

    Refactor event listener for FAQ items

--- script.js
+++ script.js
@@ -3,19 +3,9 @@ const faqItems = document.querySelectorAll('.faq-item');
 faqItems.forEach(item => {
 
   item.addEventListener('click', () => {
+
     item.classList.toggle('active');
+
   });
-
-});
-
-const buyBtn = document.getElementById('buyBtn');
-
-buyBtn.addEventListener('click', function(e){
-
-  e.preventDefault();
-
-  alert(
-    'Replace this BUY NOW button link with your Stakecut checkout URL.'
-  );
 
 });
